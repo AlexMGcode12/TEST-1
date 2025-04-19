a1a147b7b2086068bdf01bf7b2b99f2ef910461c
Alex Martingale <sermeozalot@gmail.com>
2025-04-18 21:35:33 -0230

    test 1

---
+++ test.py
@@ -0,0 +1,21 @@
+print("Welcome To The Blot Joint")
+enter_age = (16)
+print("Enter Age Is", enter_age)
+
+
+ 
+ 
+def age_check(age_enter):
+    # checks age
+    age = int(input("Enter Your Age "))
+    
+    # checks if age is greater than or equal to 18
+    if age >= enter_age:
+        print("You Are a Adult")
+    elif age <= enter_age:
+        print("You Are a Minor")
+    else:
+        print("You Need to Enter a Valid Age")
+
+# Starts The Function
+age_check(enter_age)
